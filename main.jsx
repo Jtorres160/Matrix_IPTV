61552a426233e24b3ddb0db828a330e8b80203b3
Jayslim <jtorres160@student.rccd.edu>
2025-10-31 20:01:10 -0700

    Delete main.jsx

--- main.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './supreme_layout.jsx'
-
-const root = createRoot(document.getElementById('root'))
-root.render(<App />)
-
-
